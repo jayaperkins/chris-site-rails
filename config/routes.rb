b1fd485e8813170d22ea6c69b584ca55855e0af6
jayaperkins <jayaperkins@gmail.com>
2016-05-20 15:21:41 -0700

    updated home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   get 'store/index'
 
   resources :products
 
-  root 'store#index', as: 'store'
+  root 'home#index', as: 'home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
