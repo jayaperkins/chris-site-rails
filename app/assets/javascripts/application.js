5c4d910fc031859ad20da967f68f9968b10d6501
jayaperkins <jayaperkins@gmail.com>
2016-05-20 00:09:11 -0700

    update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,6 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+$(function(){var $body=$('body'); $body.hide(0).delay(500).fadeIn(2500); var $linkContainer=$('.link-container'); $linkContainer.on('mouseover', function(){$(this).addClass('hover');}); $linkContainer.on('mouseout', function(){$(this).removeClass('hover');});});
