083ba0fb7249bb0404029d8023b282e19f569d5d
jayaperkins <jayaperkins@gmail.com>
2016-05-20 17:45:07 -0700

    updated layout

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,10 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+config.generators do |g|
+  g.assets true
+end
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
